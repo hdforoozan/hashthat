160d43650e3834d13b887130da66ac2735014926
Hadi Foroozanfar <hdforoozan@gmail.com>
2019-05-31 16:24:32 +0430

    Commnet out Functional TestCase and add Unit TestCase and wrote home page test with forms and without

--- hashing/views.py
+++ hashing/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .forms import HashForm
 
-# Create your views here.
+def home(request):
+	form  = HashForm()
+	return render(request, 'hashing/home.html', {'form':form})
