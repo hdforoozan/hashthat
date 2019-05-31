3c4ac3ebb88f981574fdc736942116f0f53bdd2c
Hadi Foroozanfar <hdforoozan@gmail.com>
2019-06-01 00:26:01 +0430

    write test for model objects and views and end the hashthat project

--- hashing/tests.py
+++ hashing/tests.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from selenium import webdriver
 from .forms import HashForm
+import hashlib
+from .models import Hash
 
 # class FunctionalTestCase(TestCase):
 # 	def setUp(self):
@@ -15,7 +17,7 @@ from .forms import HashForm
 # 		text = self.browser.find_element_by_id('id_text')
 # 		text.send_keys('hello')
 # 		self.browser.find_element_by_name('submit').click()
-# 		self.assertIn('2CF24DBA5FB0A30E26E83B2AC5B9E29E1B161E5C1FA7425E73043362938B9824',self.browser.page_source)
+# 		self.assertIn('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',self.browser.page_source)
 		
 # 	def tearDown(self):
 # 		self.browser.quit()
@@ -29,5 +31,26 @@ class UnitTestCase(TestCase):
 	def test_hash_form(self):
 		form = HashForm(data={'text':'hello'})
 		self.assertTrue(form.is_valid())
+
+	def saveHash(self):
+		Hash1 = Hash()
+		Hash1.text = 'hello'
+		Hash1.Hash = '2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824'
+		Hash1.save()
+		return Hash1
+
+	def test_hash_func_works(self):
+		text_hash = hashlib.sha256('hello'.encode('utf-8')).hexdigest()
+		self.assertEqual('2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824',text_hash)
+
+	def test_hash_object(self):
+		Hash1 = self.saveHash()
+		pulled_hash = Hash.objects.get(Hash='2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+		self.assertEqual(Hash1.text,pulled_hash.text)
+
+	def test_viewing_hash(self):
+		Hash1 = self.saveHash()
+		response = self.client.get('/hash/2cf24dba5fb0a30e26e83b2ac5b9e29e1b161e5c1fa7425e73043362938b9824')
+		self.assertContains(response,'hello')
 
 
--- hashing/views.py
+++ hashing/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .forms import HashForm
+from .models import Hash
+import hashlib
 
 def home(request):
+	if request.method == 'POST':
+		print(request.POST['text'])
+		print(request.POST['submit'])
+		filled_form = Hash(request.POST['text'],request.POST['submit'])
+		if filled_form.is_valid():
+			text = filled_form.cleaned_data['text']
+			text_hash = hashlib.sha256(text.encode('utf-8')).hexdigest()
+			try:
+				Hash.objects.get(Hash=text_hash)
+			except Hash.DoesNotExist:
+				Hash1 = Hash()
+				Hash1.text = text
+				Hash1.Hash = text_hash
+				Hash1.save()
+			return redirect('Hash',Hash=text_hash)
+
 	form  = HashForm()
 	return render(request, 'hashing/home.html', {'form':form})
+
+def Hash(request, hash1):
+	Hash1 = Hash.objects.get(Hash=hash1)
+	return render(request, 'hashing/hash.html', {'Hash':Hash1})
